f5acaa52e19c1756d5534950eb6f4159ead2f484
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-24 14:46:09 -0400

    hire me elon

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,7 +3,7 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         //a = (speed * speed)/(2 * altitude)
-        int burn = ((status.Velocity * status.Velocity)/(2 * status.Altitude));
+        int burn = (status.Velocity * status.Velocity)/(2 * status.Altitude);
         
         System.out.println(burn); /*hack!*/
         return burn;
